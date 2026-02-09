9d814aaff340103fa94068a83cbc7585fc97f156
Essence Roberts <essencenroberts@gmail.com>
2026-02-09 15:28:36 +0700

    updated time entry to use time in/time out and calculate total hours because it made more sense

--- src/components/TimeEntryForm.jsx
+++ src/components/TimeEntryForm.jsx
@@ -1,23 +1,83 @@
+import { useState } from 'react'
+
 function TimeEntryForm() {
+  const [name, setName] = useState('')
+  const [date, setDate] = useState('')
+  const [timeIn, setTimeIn] = useState('')
+  const [timeOut, setTimeOut] = useState('')
+  
+function handleSubmit(e) {
+  e.preventDefault()
+
+  const entry = {
+    name,
+    date,
+    timeIn,
+    timeOut,
+    totalHours: calculateHours(timeIn, timeOut)
+  }
+
+  console.log(entry)
+
+  setName('')
+  setDate('')
+  setTimeIn('')
+  setTimeOut('')
+}  
+
+function calculateHours (start,end) {
+  if (!start || !end) return 0
+
+  const startTime = new Date(`2026-02-02T${start}`)
+  const endTime = new Date(`2026-02-02T${end}`)
+
+  const diffMs = endTime - startTime
+  const diffHours = diffMs / (1000 * 60 * 60)
+
+  return diffHours
+}
+
   return (
     <section>
      <h2>Log Hours</h2>
       
-     <form>
+     <form onSubmit={handleSubmit}>
         <div>
           <label>Employee Name</label>
-          <input type="text" />
+          <input 
+            type="text" 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
         <div>
           <label>Date</label>
-          <input type="date" />
+          <input 
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
         </div>
 
         <div>
-          <label>Hours Worked</label>
-          <input type="number" />
+          <label>Time In</label>
+          <input 
+            type="time"
+            value={timeIn}
+            onChange={(e) => setTimeIn(e.target.value)} 
+          />
         </div>
+        
+         <div>
+          <label>Time Out</label>
+          <input 
+            type="time"
+            value={timeOut}
+            onChange={(e) => setTimeOut(e.target.value)} 
+          />
+        </div>
+
 
         <button type="submit">Add Time</button>
       </form>
