5b1b7834caeadf4f511d52d8c97de390c0224820
Essence Roberts <essencenroberts@gmail.com>
2026-02-12 21:00:25 +0700

    hide admin view import from app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header'
 import TimeEntryForm from './components/TimeEntryForm'
-import AdminView from './components/AdminView'
+/* import AdminView from './components/AdminView' */
 
 function App() {
   return (
