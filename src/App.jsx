dab2be9c8b41253fac3d2e6fc299ab7c53e22eaf
Essence Roberts <essencenroberts@gmail.com>
2026-02-16 09:11:34 +0700

    added footer with website link and glass morphsim effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header'
 import TimeEntryForm from './components/TimeEntryForm'
 /* import AdminView from './components/AdminView' */
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div>
       <Header />
       <TimeEntryForm />
+      <Footer />
 
     </div>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>
+        © {new Date().getFullYear()} Dr. Shell's Soul Food Cooking
+        
+      </p>
+      <p><a
+          href="https://drshellssoulfood.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Visit Website
+        </a></p>
+    </footer>
+  )
+}
+
+
+export default Footer
